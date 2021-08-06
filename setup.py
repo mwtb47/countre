24a12d148c46cb476273d9ee52105d17da025d68
mwtb47 <57566091+mwtb47@users.noreply.github.com>
2021-08-06 17:41:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="countre",
-    version="0.0.3",
+    version="0.0.4",
     author="Matthew Barnes",
     author_email="mwt.barnes@outlook.com",
     description="Package to get country data from country names.",
